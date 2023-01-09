48eee77c131348e780ba960ddffca00a8820ed3c
Oscar DAVASE <oscardavase@gmail.com>
2023-01-09 15:16:57 +0100

    update

--- vue.js
+++ vue.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-axios.get('https://www.data.gouv.fr/fr/reuses/penurie-de-main-doeuvre-quels-sont-les-metiers-sous-tension-spallian/')
+axios.get('https://www.data.gouv.fr/fr/reuses/penurie-de-main-doeuvre-quels-sont-les-metiers-sous-tension-spallian/', {
+    headers: {
+        'Authorization': 'Bearer 60effecd9bf705a0342d1163'
+    }
+})
     .then(response => {
         // Traitez la réponse de l'API ici
     })
